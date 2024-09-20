4e30c85b25d84b85e2a6425470a94e9189c5e69c
hayashi3017 <kuro.love.3017ch@gmail.com>
2024-09-20 23:18:07 +0900

    feat: add react router

---
+++ react/vite-react-playground/src/Button.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { CommonButton } from "./components/button/CommonButton";
+import { CommonForm } from "./components/form/CommonForm";
+
+function Button() {
+  const [count, setCount] = useState(0);
+  function increment_count() {
+    setCount((count) => count + 1);
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <CommonButton onClick={increment_count} disabledOnClick={false}>
+          <>count is {count}</>
+        </CommonButton>
+        <CommonForm onSubmit={increment_count}></CommonForm>
+        <p>
+          Edit <code>src/Button.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default Button;
--- react/vite-react-playground/src/main.tsx
+++ react/vite-react-playground/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Button from "./Button.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="button" element={<Button />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
