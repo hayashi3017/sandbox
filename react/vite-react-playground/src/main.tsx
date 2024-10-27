58878e707af8df222e3b1b64c8d927b1b1d540f0
hayashi3017 <kuro.love.3017ch@gmail.com>
2024-10-27 22:35:14 +0900

    add: button double click case with msw

--- react/vite-react-playground/src/main.tsx
+++ react/vite-react-playground/src/main.tsx
@@ -22,8 +22,22 @@ const router = createBrowserRouter(
   )
 );
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+});
--- react/vite-react-playground/src/pages/Button/ButtonPage.tsx
+++ react/vite-react-playground/src/pages/Button/ButtonPage.tsx
@@ -2,19 +2,51 @@ import { useState } from "react";
 import classes from "./ButtonPage.module.css";
 import { CommonButton } from "../../components/button/CommonButton";
 import { CommonForm } from "../../components/form/CommonForm";
+import { useNavigate } from "react-router-dom";
 
 function ButtonPage() {
+  const navigate = useNavigate();
   const [count, setCount] = useState(0);
+  const [disabledClick, setDisabledClick] = useState(false);
   function increment_count() {
+    setDisabledClick(true);
     setCount((count) => count + 1);
+    setDisabledClick(false);
+  }
+
+  async function onClickButton() {
+    setDisabledClick(true);
+    handle_request();
+    setDisabledClick(false);
+  }
+
+  async function handle_request() {
+    await some_request();
+    goToScriptPage();
+  }
+
+  async function some_request() {
+    const response = await fetch("https://example.com/user");
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function goToScriptPage() {
+    navigate("/script_sync_example");
+    // setTimeout(() => {
+    //   navigate("/script_sync_example");
+    // }, 3000);
   }
 
   return (
     <>
       <h1>Button Examples</h1>
       <div className={classes.card}>
-        <CommonButton onClick={increment_count} disabledOnClick={false}>
+        <CommonButton onClick={increment_count} disabledOnClick={disabledClick}>
           <>count is {count}</>
+        </CommonButton>
+        <CommonButton onClick={onClickButton} disabledOnClick={disabledClick}>
+          <>start some request</>
         </CommonButton>
         <CommonForm onSubmit={increment_count}></CommonForm>
         <p>Explore some pages!</p>
