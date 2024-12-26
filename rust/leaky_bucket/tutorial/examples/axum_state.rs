0ed1f8d4e2aea47dcc8aac86d336264310adeb2d
hayashi3017 <kuro.love.3017ch@gmail.com>
2024-12-26 23:07:43 +0900

    fix: add log

--- rust/leaky_bucket/tutorial/examples/axum_state.rs
+++ rust/leaky_bucket/tutorial/examples/axum_state.rs
@@ -8,16 +8,16 @@ use axum::{
     Router,
 };
 use leaky_bucket::RateLimiter;
-use tokio::{sync::Mutex, time};
+use tracing::{debug, level_filters::LevelFilter};
 
 struct AppState {
-    volume: Arc<RateLimiter>,
+    tokens: Arc<RateLimiter>,
 }
 
 impl AppState {
     pub fn new() -> AppState {
         AppState {
-            volume: Arc::new(
+            tokens: Arc::new(
                 RateLimiter::builder()
                     .initial(5)
                     .interval(Duration::from_secs(1))
@@ -31,41 +31,37 @@ impl AppState {
 
 #[tokio::main]
 async fn main() {
+    use tracing_subscriber::prelude::*;
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::builder()
+                .with_default_directive(LevelFilter::DEBUG.into())
+                .from_env_lossy(),
+        )
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_target(false)
+                .with_level(true)
+                .compact(),
+        )
+        .init();
+
     let state = Arc::new(AppState::new());
 
-    // トークン数の状態をログに出力するバックグラウンドタスク
-    // let state_clone = state.clone();
-    // tokio::spawn(async move {
-    //     loop {
-            // let state_clone = state_clone.lock().await;
-            // balance()はrefill()された状態を反映しない様子、つまり現時点の残りのサイズを示さない
-            // println!("Current tokens: {}", state_clone.volume.balance());
-            // drop(state_clone); // ロックを解放
-    //         time::sleep(Duration::from_secs(1)).await;
-    //     }
-    // });
-
-    // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/volume", post(volumes))
+        .route("/bucket", post(use_bucket))
         .with_state(state);
 
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-
-    // for i in 1..=10 {
-    //     // トークンを1つ消費
-    //     state.volume.acquire_one().await;
-    //     println!("Task {} completed.", i);
-    // }
 }
 
-async fn volumes(State(state): State<Arc<AppState>>) -> Response {
-    // let state = state.lock().await;
-    if !state.volume.try_acquire(1) {
-        return (StatusCode::TOO_MANY_REQUESTS, "No milk available\n").into_response();
+async fn use_bucket(State(state): State<Arc<AppState>>) -> Response {
+    debug!("API called.");
+    if !state.tokens.try_acquire(1) {
+        return (StatusCode::TOO_MANY_REQUESTS, "No token left.\n").into_response();
     }
-    (StatusCode::OK, "Milk withdrawn\n").into_response()
+    (StatusCode::OK, "Token used.\n").into_response()
 }
